106b9f2b2c77a677bc36364f9b2ae4e418787802
devvime <dihapp@outlook.com>
2024-04-05 01:18:18 -0300

    fix structure

--- server.js
+++ server.js
@@ -1,30 +1,86 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const fs = require('fs')
-const { v4: uuidv4 } = require('uuid')
+const express = require('express'); 
+const app = express(); 
+const server = require('http').Server(app); 
+const fs = require('fs'); 
+server.listen(process.env.PORT || 8080);
 
-let username, password
-
-server.listen(process.env.PORT || 8080)
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-
-app.get('/', (req, res) => {
-  res.render('frontpage')
+app.use(express.static('public')); 
+app.set('view engine', 'ejs'); 
+app.get('/', (req, res) => { 
+  res.render('frontpage'); 
 })
 
-app.get('/newRoom', (req, res) => {
-  username = req.query.username
-  password = req.query.password
-  let roomId = uuidv4()
-  fs.appendFileSync("public/logs.txt", roomId + ":" + password + "\n", "utf-8")
-  res.redirect(`/${roomId}`)
+const { v4: uuidv4 } = require('uuid');
+var un, pc;
+app.get('/newroom', (req, res) => {
+  un = req.query.username;
+  pc = req.query.password;
+  var roomId = uuidv4();
+  fs.appendFileSync("public/meeting-log.txt", roomId + ":" + pc + "\n", "utf-8");
+  res.redirect(`/${roomId}`);
 })
+
+var unJ, inJ, pcJ;
+app.get('/joinroom', (req, res) => {
+  unJ = req.query.username;
+  inJ = req.query.invitation;
+  pcJ = req.query.password;
+  var log = fs.readFileSync("public/meeting-log.txt", "utf-8");
+  var findInvitation = log.indexOf(inJ + ":" + pcJ);
+  if (findInvitation != -1) {
+    res.redirect(`/${inJ}`);
+    un = unJ,
+      pc = pcJ
+  } else {
+    var findInvitation = log.indexOf(inJ);
+    if (findInvitation == -1) {
+      res.send("Invalid invitation. Please <a href=/>go back</a>");
+    } else {
+      var findpassword = log.indexOf(inJ + ":" + pcJ);
+      if (findpassword == -1) {
+        res.send("Invalid password. Please <a href=/>go back</a>");
+      }
+    }
+  }
+});
 
 app.get('/:room', (req, res) => {
   res.render('meeting-room', {
     roomId: req.params.room,
-    userName: username
+    username: un,
+  });
+})
+
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+app.use('/peerjs', peerServer);
+
+const io = require('socket.io')(server);
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, peerId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', peerId);
+
+    socket.on('stop-screen-share', (peerId) => {
+      io.to(roomId).emit('no-share', peerId);
+    })
+
+    socket.on('message', (message, sender, color, time) => {
+      io.to(roomId).emit('createMessage', message, sender, color, time);
+    })
+
+    socket.on('leave-meeting', (peerId, peerName) => {
+      io.to(roomId).emit('user-leave', peerId, peerName);
+    })
   })
 })
+
+app.post('/upload', (req, res) => {
+  const fileName = req.headers['file-name'];
+  req.on('data', (chunk) => {
+    fs.appendFileSync(__dirname + '/public/uploads/' + fileName, chunk);
+  })
+  res.end('uploaded');
+});
